309e4c9d007049c65ecd45f90352132d4b438fd7
Rod Kristian Rebarbas <50227721+kryss143@users.noreply.github.com>
2026-06-05 17:07:12 +0800

    Update kanban page

--- app-tracker/app/kanban/page.tsx
+++ app-tracker/app/kanban/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 import { getApplications } from "@/actions/applications";
 import KanbanBoard from "@/components/KanbanBoard";
+import Navbar from "@/components/Navbar";
 
 export const dynamic = "force-dynamic";
 
@@ -16,8 +17,11 @@ export default async function KanbanPage() {
   const applications = await getApplications();
 
   return (
-    <div className="max-w-400 mx-auto px-4 md:px-8 pt-24 pb-12">
-      <KanbanBoard initialApplications={applications} />
-    </div>
+    <>
+      <Navbar userEmail={user.email ?? ""} />
+      <main className="max-w-400 mx-auto px-4 md:px-8 pt-24 pb-12">
+        <KanbanBoard initialApplications={applications} />
+      </main>
+    </>
   );
 }
